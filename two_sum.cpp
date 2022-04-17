a391a3fd46642995fbd056b922625fe89fc5c1de
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-04-16 21:30:39 -0700

    Update two_sum.cpp
    Added comments

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,3 +1,18 @@
+/*
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+*****************************************************************************************************************************
+
+Runtime: 316 ms, faster than 36.84% of C++ online submissions for Two Sum.
+Memory Usage: 10.2 MB, less than 66.64% of C++ online submissions for Two Sum.
+
+*/
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
