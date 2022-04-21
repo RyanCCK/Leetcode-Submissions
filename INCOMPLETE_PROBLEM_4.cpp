a3c1353de01b929f518ee9e831795ec59a9e34ac
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-04-21 14:48:23 -0700

    Update INCOMPLETE_PROBLEM_4.cpp
    Still Not Working
    
    Runtime error on input:
    nums1 = []
    nums2 - [1]

--- INCOMPLETE_PROBLEM_4.cpp
+++ INCOMPLETE_PROBLEM_4.cpp
@@ -3,23 +3,29 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int nums1Size = nums1.size();
         int nums2Size = nums2.size();
-        double nums1Med, nums1Index, nums2Index, median, shift;
+        double nums1Med, nums1Index, nums2Index, shift;
+        double median=0;
         
         //Get median and index of median for nums1
         nums1Index = (nums1Size/2)-0.5;
-        if(nums1Index-floor(nums1Index) == 0)
+        if(!nums1.empty() && nums1Index-floor(nums1Index) == 0)
             nums1Med = nums1[nums1Index];
-        else nums1Med = (nums1[ceil(nums1Index)]+nums1[floor(nums1Index)])/2;
+        else if(!nums1.empty())
+            nums1Med = (nums1[ceil(nums1Index)]+nums1[floor(nums1Index)])/2;
         
         //Find highest index in nums2 with value less than median of nums1
         nums2Index = nums2Size-1;
-        while(nums2Index>=0 && nums2[nums2Index]>=nums1Med)
-            nums2Index--;
+        if(!nums2.empty())
+        {
+            while(nums2Index>=0 && nums2[nums2Index]>=nums1Med)
+                nums2Index--;
+        }
         
         //Set current median, shift, and valid indices
         double nextElement;
         median = nums1Med;
         shift = (nums2Size - 2*(nums2Index+1))/2;
+        //Prevent out-of-bounds access on nums2
         if(nums2Index<0) nums2Index = 0;
         if(nums1Index-floor(nums1Index) != 0)
         {
@@ -35,34 +41,72 @@ public:
             }
         }
         
+        //Used for bounds checking on array indices
+        bool nums1OOB = false;
+        bool nums2OOB = false;
+        
         //Find median in hypothetical merged array
         while(shift<=-1 || shift>=1)
         {
             if(shift <= -1)
             {
-                if(nums1[nums1Index] > nums2[nums2Index])
+                if(!nums1OOB && !nums2OOB)
                 {
-                    median = nums1[nums1Index];
-                    --nums1Index;
+                    if(nums1[nums1Index] > nums2[nums2Index])
+                    {
+                        median = nums1[nums1Index];
+                        --nums1Index;
+                        if(nums1Index<0) nums1OOB = true;
+                    }
+                    else
+                    {
+                        median = nums2[nums2Index];
+                        --nums2Index;
+                        if(nums2Index<0) nums2OOB = true;
+                    } 
                 }
-                else
+                else if(nums1OOB && !nums2OOB)
                 {
                     median = nums2[nums2Index];
                     --nums2Index;
+                    if(nums2Index<0) nums2OOB = true;
+                }
+                else if(nums2OOB && !nums1OOB)
+                {
+                    median = nums1[nums1Index];
+                    --nums1Index;
+                    if(nums1Index<0) nums1OOB = true;
                 }
                 ++shift;
             }
             else if(shift >= 1)
             {
-                if(nums1[nums1Index] < nums2[nums2Index])
+                if(!nums1OOB && !nums2OOB)
                 {
-                    median = nums1[nums1Index];
-                    ++nums1Index;
+                    if(nums1[nums1Index] < nums2[nums2Index])
+                    {
+                        median = nums1[nums1Index];
+                        ++nums1Index;
+                        if(nums1Index>=nums1Size) nums1OOB = true;
+                    }
+                    else
+                    {
+                        median = nums2[nums2Index];
+                        ++nums2Index;
+                        if(nums2Index>=nums2Size) nums2OOB = true;
+                    }
                 }
-                else
+                else if(nums1OOB && !nums2OOB)
                 {
                     median = nums2[nums2Index];
                     ++nums2Index;
+                    if(nums2Index>=nums2Size) nums2OOB = true;
+                }
+                else if(nums2OOB && !nums1OOB)
+                {
+                    median = nums1[nums1Index];
+                    ++nums1Index;
+                    if(nums1Index>=nums1Size) nums1OOB = true;
                 }
                 --shift;
             }
@@ -71,93 +115,29 @@ public:
         //If median is between two numbers, calculate median
         if(shift == -0.5)
         {
-            bool nums1OOB = false;
-            bool nums2OOB = false;
-            
-            //Bounds correcting
-            if(nums1Index >= nums1Size)
+            if(!nums1OOB && !nums2OOB)
             {
-                nums1Index = nums1Size-1;
-                nums1OOB = true;
+                if(nums1[nums1Index] > nums2[nums2Index]) 
+                    median = (median+nums1[nums1Index])/2;
+                else median = (median+nums2[nums2Index])/2;
             }
-            else if(nums1Index < 0)
-            {
-                nums1Index = 0;
-                nums1OOB = true;
-            }
-            if(nums2Index >= nums2Size)
-            {
-                nums2Index = nums2Size-1;
-                nums2OOB = true;
-            }
-            else if(nums2Index < 0)
-            {
-                nums2Index = 0;
-                nums2OOB = true;
-            }
-            
-            if(!nums1OOB && nums1[nums1Index] > nums2[nums2Index])
-            {
-                median = (median+nums1[nums1Index])/2;
-            }
-            //the first condition here might be unnecessary.
-            else if(nums1OOB && nums1[nums1Index] > nums2[nums2Index])
-            {
+            else if(nums1OOB && !nums2OOB)
                 median = (median+nums2[nums2Index])/2;
-            }
-            else if(!nums2OOB)
-            {
-                median = (median+nums2[nums2Index])/2;
-            }
-            else
-            {
+            else if(nums2OOB && !nums1OOB)
                 median = (median+nums1[nums1Index])/2;
-            }
         }
         else if(shift == 0.5)
         {
-            bool nums1OOB = false;
-            bool nums2OOB = false;
-            
-            //Bounds correcting
-            if(nums1Index >= nums1Size)
+            if(!nums1OOB && !nums2OOB)
             {
-                nums1Index = nums1Size-1;
-                nums1OOB = true;
+                if(nums1[nums1Index] < nums2[nums2Index]) 
+                    median = (median+nums1[nums1Index])/2;
+                else median = (median+nums2[nums2Index])/2;
             }
-            else if(nums1Index < 0)
-            {
-                nums1Index = 0;
-                nums1OOB = true;
-            }
-            if(nums2Index >= nums2Size)
-            {
-                nums2Index = nums2Size-1;
-                nums2OOB = true;
-            }
-            else if(nums2Index < 0)
-            {
-                nums2Index = 0;
-                nums2OOB = true;
-            }
-            
-            if(!nums1OOB && nums1[nums1Index] < nums2[nums2Index])
-            {
-                median = (median+nums1[nums1Index])/2;
-            }
-            //the first condition here might be unnecessary.
-            else if(nums1OOB && nums1[nums1Index] < nums2[nums2Index])
-            {
+            else if(nums1OOB && !nums2OOB)
                 median = (median+nums2[nums2Index])/2;
-            }
-            else if(!nums2OOB)
-            {
-                median = (median+nums2[nums2Index])/2;
-            }
-            else
-            {
+            else if(nums2OOB && !nums1OOB)
                 median = (median+nums1[nums1Index])/2;
-            }
         }
         
         return median;
