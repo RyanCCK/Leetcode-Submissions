e98e527a0f61f5b696a481e81c2d23535b8eef47
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-04-21 23:16:09 -0700

    Update INCOMPLETE_PROBLEM_4.cpp
    STILL NOT WORKING.
    
    Fails on test case:
    nums1 = [1,2]
    nums2 = [1,2,3]
    
    Output: 1
    Expected: 2

--- INCOMPLETE_PROBLEM_4.cpp
+++ INCOMPLETE_PROBLEM_4.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int nums1Size = nums1.size();
-        int nums2Size = nums2.size();
-        double nums1Med, nums1Index, nums2Index, shift;
-        double median=0;
+        double nums1Size = nums1.size();
+        double nums2Size = nums2.size();
+        double nums1Med, nums1Index, nums2Index, median, shift;
+        
+        //Guard against empty arrays
+        if(nums1.empty())
+        {
+            if(fmod((nums2Size/2.0), 1) == 0)
+                median = (nums2[nums2Size/2.0]+nums2[(nums2Size/2.0)-1])/2.0;
+            else median = nums2[floor(nums2Size/2.0)];
+            return median;
+        }
+        else if(nums2.empty())
+        {
+            if(fmod((nums1Size/2.0), 1) == 0)
+                median = (nums1[nums1Size/2.0]+nums1[(nums1Size/2.0)-1])/2.0;
+            else median = nums1[floor(nums1Size/2.0)];
+            return median;
+        }
         
         //Get median and index of median for nums1
-        nums1Index = (nums1Size/2)-0.5;
-        if(!nums1.empty() && nums1Index-floor(nums1Index) == 0)
+        nums1Index = (nums1Size/2.0)-0.5;
+        if(nums1Index-floor(nums1Index) == 0)
             nums1Med = nums1[nums1Index];
-        else if(!nums1.empty())
-            nums1Med = (nums1[ceil(nums1Index)]+nums1[floor(nums1Index)])/2;
+        else nums1Med = (nums1[ceil(nums1Index)]+nums1[floor(nums1Index)])/2.0;
         
         //Find highest index in nums2 with value less than median of nums1
         nums2Index = nums2Size-1;
-        if(!nums2.empty())
-        {
-            while(nums2Index>=0 && nums2[nums2Index]>=nums1Med)
-                nums2Index--;
-        }
+        while(nums2Index>=0 && nums2[nums2Index]>=nums1Med)
+            nums2Index--;
+        
+        //Used for bounds checking on array indices
+        bool nums1OOB = false;
+        bool nums2OOB = false;
         
         //Set current median, shift, and valid indices
         double nextElement;
         median = nums1Med;
-        shift = (nums2Size - 2*(nums2Index+1))/2;
+        shift = (nums2Size - 2*(nums2Index+1))/2.0;
         //Prevent out-of-bounds access on nums2
         if(nums2Index<0) nums2Index = 0;
+        //Update nums1Index for correct comparisons
         if(nums1Index-floor(nums1Index) != 0)
         {
             if(shift<0) 
@@ -40,10 +56,16 @@ public:
                 shift += 0.5;
             }
         }
-        
-        //Used for bounds checking on array indices
-        bool nums1OOB = false;
-        bool nums2OOB = false;
+        else
+        {
+            if(shift<0)
+                nums1Index--;
+            else if(shift>0)
+                nums1Index++;
+            
+            if(nums1Index<0 || nums1Index>=nums1Size)
+                nums1OOB = true;
+        }
         
         //Find median in hypothetical merged array
         while(shift<=-1 || shift>=1)
