1b4955715d5d47be08db4bedc2b1e6cdb2915ff0
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-05-06 12:18:05 -0700

    Update and rename INCOMPLETE_PROBLEM_11.cpp to 11__container_with_most_water.cpp
    Slow, expensive solution.

--- 11__container_with_most_water.cpp
+++ 11__container_with_most_water.cpp
@@ -1,3 +1,25 @@
+/*
+
+LEETCODE PROBLEM 11 | Container With Most Water | 5/6/2022
+
+*****************************************************************************************************************************
+
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints 
+of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+*****************************************************************************************************************************
+
+Runtime: 1506 ms, faster than 5.01% of C++ online submissions for Container With Most Water.
+Memory Usage: 59 MB, less than 39.29% of C++ online submissions for Container With Most Water.
+
+*/
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
@@ -15,17 +37,17 @@ public:
         for(int i=0; i<height.size(); ++i)
         {
             //Set terminal index for current front index
-            terminalIndex = ((maxArea/height[i])-1)+i;
+            terminalIndex = (height[i] != 0) ? ((maxArea/height[i])-1)+i : i+1;
             //Inner loop to set back index
-            for(int j=height.size(); j>i; --j)
+            for(int j=height.size()-1; j>i; --j)
             {
                 //Exit loop if past terminal index
                 if(j <= terminalIndex) break;
                 //Update inner loop values
-                length = abs(i-j)+1;
+                length = abs(i-j);
                 currArea = (height[i] < height[j]) ? height[i] * length : height[j] * length;
                 //Update terminal index (if possible) based on current area
-                if(((currArea/height[i])-1)+i > terminalIndex) 
+                if(height[i] != 0 && ((currArea/height[i])-1)+i > terminalIndex) 
                     terminalIndex = ((currArea/height[i])-1)+i;
                 //If current area is greater than maxArea, update maxArea
                 if(currArea > maxArea)
