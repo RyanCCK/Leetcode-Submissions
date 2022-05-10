04afd33eade634fa4bb8d78f5c3b66fb37297511
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-05-10 12:26:47 -0700

    Update INCOMPLETE_PROBLEM_15.cpp
    Time Limit Exceeded on test case 315/318.
    
    Will revisit this problem in the future to attempt a faster solution.

--- INCOMPLETE_PROBLEM_15.cpp
+++ INCOMPLETE_PROBLEM_15.cpp
@@ -14,82 +14,38 @@ public:
             return triplets;
         }
         
-        //Sort nums in non-descending order
-        vector<int> sortedNums(nums);
-        sort(sortedNums.begin(), sortedNums.end());
+        //Map to increase speed of element lookup. Value is index, key is value in nums at that index.
+        unordered_map<int, int> umap;
+        for(int i=0; i<nums.size(); ++i)
+            umap[nums[i]] = i;
         
-        //Check if all values are negative
-        if(sortedNums[sortedNums.size()-1] < 0) return triplets;
-        
-        //Determine amount of 0s, and the indices of the first and last occurrences. 
-        int numZeros = 0, firstZeroIndex = 0, lastZeroIndex;
-        while(firstZeroIndex < sortedNums.size() && sortedNums[firstZeroIndex] != 0)
-            ++firstZeroIndex;
-        if(firstZeroIndex != sortedNums.size())
-        {
-            lastZeroIndex = firstZeroIndex;
-            while(lastZeroIndex < sortedNums.size() && sortedNums[lastZeroIndex] == 0)
-            {
-                ++numZeros;
-                ++lastZeroIndex;
-            }
-            --lastZeroIndex;    //This always ends up 1 past the actual index of the last 0
-        }
-        //Based on number of 0s:
-        //  If 0 or 1, do nothing.
-        //  If 2, remove 1.
-        //  If 3 or greater, append {0,0,0} to triplets, then remove all but 1.
-        switch(numZeros)
-        {
-            case 0:
-                //Not really firstZeroIndex; we are finding index of first non-negative value. Will be assigned to correct name further below.
-				for(int i=0; i<sortedNums.size(); ++i)
-				{
-					if(sortedNums[i] >= 0) 
-					{
-						firstZeroIndex = i;
-						break;
-					}
-				}
-                break;
-            case 1:
-                break;
-            case 2:
-                sortedNums.erase(next(sortedNums.begin(), lastZeroIndex));
-                break;
-            default:
-                triplets.push_back({0,0,0});
-                sortedNums.erase(next(sortedNums.begin(), firstZeroIndex+1), next(sortedNums.begin(), lastZeroIndex+1));
-        }
-        
-        //Perform search and populate solution vector.
-        // Search is with respect to partition: firstZeroIndex
+        //Temporary vector to hold a triplet
         vector<int> temp;
-        bool dupFound = false;
-        //There may be no zeroes. This is a more accurate variable name.
-		int firstNonNegativeIndex = firstZeroIndex;
-        for(int index1=0; index1<firstNonNegativeIndex; ++index1)
+        
+        //Fix first element, then fix second element, then use map to lookup if valid third element exists.
+        //  If so, ensure that it's not a duplicate of either of the first two.
+        int diff;
+        for(int index1=0; index1<nums.size()-1; ++index1)
         {
-            for(int index2=index1+1; index2<sortedNums.size()-1; ++index2)
+            for(int index2=index1+1; index2<nums.size(); ++index2)
             {
-                int index3 = (index2<firstNonNegativeIndex) ? 
-                    firstNonNegativeIndex : index2 + 1;
-                while(index3 < sortedNums.size())
+                diff = -1 * (nums[index1] + nums[index2]);
+                //If third value is found in the map
+                if(umap.count(diff) != 0)
                 {
-                    //If valid triplet candidate is found, check if it already exists within triplets.
-                    //  If not, append it to triplets.
-                    if(sortedNums[index1] + sortedNums[index2] + sortedNums[index3] == 0)
+                    //If index of third value is different from index1 and index2
+                    if(umap.find(diff) -> second != index1 && umap.find(diff) -> second != index2)
                     {
-                        temp = {sortedNums[index1], sortedNums[index2], sortedNums[index3]};
+                        temp = {nums[index1], nums[index2], diff};
                         sort(temp.begin(), temp.end());
-                        for(int i=0; i<triplets.size() && !dupFound; ++i)
+                        //Check that this is not a duplicate triplet
+                        bool duplicate = false;
+                        for(int i=0; i<triplets.size() && !duplicate; ++i)
                         {
-                            if(triplets[i] == temp) dupFound = true;
+                            if(temp == triplets[i]) duplicate = true;
                         }
-                        if(!dupFound) triplets.push_back(temp);
-                        dupFound = false;
+                        if(!duplicate) triplets.push_back(temp);
                     }
-                    ++index3;
                 }
             }
         }
