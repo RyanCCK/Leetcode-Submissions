134565ccc33aa6dcc6cbf84685af72333632ae91
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-05-11 22:14:59 -0700

    Update 0004__median_of_two_sorted_arrays.cpp

--- 0004__median_of_two_sorted_arrays.cpp
+++ 0004__median_of_two_sorted_arrays.cpp
@@ -8,6 +8,16 @@ Given two sorted arrays nums1 and nums2 of size m and n respectively, return the
 
 The overall run time complexity should be O(log (m+n)).
 
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+
 ***********************************************************************************************************************
 
 Runtime: 55 ms, faster than 47.24% of C++ online submissions for Median of Two Sorted Arrays.
